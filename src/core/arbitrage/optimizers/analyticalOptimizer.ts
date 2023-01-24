3b0a9b106eebc6996675ec8ce56a13cac278c520
0xBossanova <0xBossanova@proton.me>
2023-01-24 08:44:45 +0100

    feat: implement cyclical optimizer

--- src/core/arbitrage/optimizers/analyticalOptimizer.ts
+++ src/core/arbitrage/optimizers/analyticalOptimizer.ts
@@ -181,3 +181,50 @@ export function getOptimalTradeForPath(path: Path, offerAssetInfo: AssetInfo): [
 	// # Return the floor of delta_a
 	return [delta_a, profit];
 }
+
+/** Given an ordered route, calculate the optimal amount into the first pool that maximizes the profit of swapping through the route
+*	Implements n-pool cylic arb from this paper: https://arxiv.org/abs/2105.02784.
+*	Extends algo to have varying swap fees for each pool.
+    @param path Path of type `Path` to calculate the optimal tradesize for.
+	@param offerAssetInfo OfferAsset type `AssetInfo` from which the arbitrage path starts. 
+    @returns [optimal tradesize, expected profit] for this particular path.
+ */
+export function getOptimalTradeForPath(path: Path, offerAssetInfo: AssetInfo): [number, number] {
+	const assetBalances = [];
+	let offerAssetNext = offerAssetInfo;
+	for (let i = 0; i < path.pools.length; i++) {
+		const [inAsset, outAsset] = getAssetsOrder(path.pools[i], offerAssetNext) ?? [];
+		offerAssetNext = outAsset.info;
+		assetBalances.push([+inAsset.amount, +outAsset.amount]);
+	}
+
+	// # Set the aprime_in and aprime_out to the first pool in the route
+	let [aprime_in, aprime_out] = assetBalances[0];
+
+	// # Set the r1_first and r2_first to the first pool in the route
+	const [r1_first, r2_first] = [1 - path.pools[0].inputfee / 100, 1 - path.pools[0].outputfee / 100];
+
+	// # Iterate through the route
+	for (let i = 1; i < assetBalances.length; i++) {
+		// # Set the a_in and a_out to the current pool in the route
+		const [a_in, a_out] = assetBalances[i];
+		// # Set the r1 and r2 to the current pool in the route
+		const [r1, r2] = [1 - path.pools[i].inputfee / 100, 1 - path.pools[i].outputfee / 100];
+		// # Calculate the aprime_in
+		aprime_in = (aprime_in * a_in) / (a_in + r1 * r2 * aprime_out);
+		// # Calculate the aprime_out
+		aprime_out = (r1 * r2 * aprime_out * a_out) / (a_in + r1 * r2 * aprime_out);
+	}
+	// # Calculate the delta_a
+	const delta_a = Math.floor((Math.sqrt(r1_first * r2_first * aprime_in * aprime_out) - aprime_in) / r1_first);
+
+	let currentOfferAsset = { amount: String(delta_a), info: offerAssetInfo };
+	for (let i = 0; i < path.pools.length; i++) {
+		const [outAmount, outInfo] = outGivenIn(path.pools[i], currentOfferAsset);
+		currentOfferAsset = { amount: String(outAmount), info: outInfo };
+	}
+	const profit = +currentOfferAsset.amount - delta_a;
+
+	// # Return the floor of delta_a
+	return [delta_a, profit];
+}
