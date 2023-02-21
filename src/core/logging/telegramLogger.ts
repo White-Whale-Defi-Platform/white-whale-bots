7dff58fcfd3fc8aaa9b730c56bbd2c530e423d77
zJ <zzju@protonmail.com>
2023-02-21 22:20:31 +0100

    eslint telegram logger

--- src/core/logging/telegramLogger.ts
+++ src/core/logging/telegramLogger.ts
@@ -1,37 +1,36 @@
 import axios from "axios";
-
 /**
  *
  */
 export class TelegramLogger {
-	private token: String;
-	private chatId: String;
+	private token: string;
+	private chatId: string;
 
 	/**
 	 *
 	 */
 	constructor(token: string, chatId: string) {
 		this.token = token;
-		this.chatId = chatId
+		this.chatId = chatId;
 	}
 
 	/**
 	 * Sends the `message` to the telegram chat if token and chatid supplied.
 	 * @param message The message to send.
-	 * @returns boolean if successful
+	 * @returns Boolean if successful.
 	 */
 	async sendMessage(message: string) {
 		if (this.token && this.chatId) {
 			try {
 				const { status } = await axios.get(`https://api.telegram.org/bot${this.token}/sendMessage`, { params: { chat_id: this.chatId, text: message } })
 				if (status === 200) {
-					return true
+					return true;
 				} else {
-					return false
+					return false;
 				}
 			} catch (e: any) {
 				console.log("error sending telegram message " + e.code);
-				return false
+				return false;
 			}
 		}
 	}
