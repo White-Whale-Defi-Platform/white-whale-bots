507b346eab94a201bd1df88bb23960cefc357b69
zJ <zzju@protonmail.com>
2023-02-26 22:56:14 +0100

    Add tests for telegram logger

---
+++ src/tests/core/logging/telegramLogger.spec.ts
@@ -0,0 +1,25 @@
+import { TelegramLogger } from "../../../core/logging/telegramLogger";
+import { describe } from 'mocha';
+import { assert } from 'chai'
+import dotenv from "dotenv";
+
+// load env files
+dotenv.config();
+const testChatId = process.env.TELEGRAM_CHAT_ID ? process.env.TELEGRAM_CHAT_ID : ""
+const testToken = process.env.TELEGRAM_BOT_TOKEN ? process.env.TELEGRAM_BOT_TOKEN : ""
+
+describe("Test send successful Telegram Message", () => {
+    it("Should return true after sending to chat", async () => {
+        const logger = new TelegramLogger(testToken, testChatId)
+        const status = await logger.sendMessage("test")
+        assert.equal(status, true)
+    })
+})
+
+describe("Test send Telegram Message with invalid data", () => {
+    it("Should return false and print error message", async () => {
+        const logger = new TelegramLogger("invalid", "00000")
+        const status = await logger.sendMessage("test")
+        assert.equal(status, false)
+    })
+})
