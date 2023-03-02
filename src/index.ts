ff9c11ae1b1c4edb91a39fc704ef9ccb3d98a8b9
nick134 <nick134-bit@proton.me>
2023-03-02 23:10:24 +0100

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,6 @@ Connections Details:\n
 	const allPools = await initPools(botClients, botConfig.poolEnvs, botConfig.mappingFactoryRouter);
 	const graph = newGraph(allPools);
 	const paths = getPaths(graph, botConfig.offerAssetInfo, botConfig.maxPathPools) ?? [];
-
 	const filteredPools = removedUnusedPools(allPools, paths);
 	setupMessage += `**\nDerived Paths for Arbitrage:
 Total Paths:** \t${paths.length}\n`;
@@ -104,7 +103,7 @@ Total Paths:** \t${paths.length}\n`;
 			skipClient,
 			skipSigner,
 			logger,
-			paths,
+			[...paths],
 		);
 	} else if (botConfig.useMempool === true) {
 		await logger.sendMessage("Initializing mempool loop...", LogType.Console);
@@ -119,7 +118,7 @@ Total Paths:** \t${paths.length}\n`;
 			account,
 			botConfig,
 			logger,
-			paths,
+			[...paths],
 		);
 	} else {
 		await logger.sendMessage("**Info:** loop without mempool or skip not implemented yet");
