4d7eac70103a0de6fb803b3480906cfc98e345d3
0xBossanova <0xBossanova@proton.me>
2023-10-31 11:05:37 +0100

    chore: remove fee logging

--- src/core/chainOperator/chainoperator.ts
+++ src/core/chainOperator/chainoperator.ts
@@ -109,7 +109,6 @@ export class ChainOperator {
 		fee?: StdFee | "auto",
 		memo?: string | undefined,
 	): Promise<TxResponse> {
-		console.log(fee);
 		try {
 			return await this.client.signAndBroadcast(msgs, fee, memo);
 		} catch (e) {
